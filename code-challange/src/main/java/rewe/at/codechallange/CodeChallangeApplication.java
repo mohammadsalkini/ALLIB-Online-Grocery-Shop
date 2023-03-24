6f1002b8825af831c1bc20604eb9dfd65b52f115
Mohammad Alsalkini <eng.salkini@hotmail.com>
2023-03-24 08:16:41 +0100

    Add initial commit

---
+++ code-challange/src/main/java/rewe/at/codechallange/CodeChallangeApplication.java
@@ -0,0 +1,13 @@
+package rewe.at.codechallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChallangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeChallangeApplication.class, args);
+    }
+
+}
---
+++ code-challange/src/test/java/rewe/at/codechallange/CodeChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package rewe.at.codechallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChallangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
